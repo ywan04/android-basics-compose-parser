6ee1538bf1898d8a9d9d338807b3f7a910f7888e
Ivan Bereza <iv.bereza04@gmail.com>
2025-04-04 18:28:02 +0300

    divided structure

--- main.go
+++ main.go
@@ -5,27 +5,35 @@ import (
 	"strconv"
 	"time"
 
-	"encoding/json"
+	//"encoding/json"
 
 	"github.com/go-rod/rod"
 )
 
 type TestData struct {
-	units []struct {
-		unitName string
-		pathways []struct {
-			pathwayName string
-			questions []struct {
-				questionTitle string
-				questionSubTitle string
-				questionType string
-				options []struct {
-					optionTitle string
-					correctness string
-				}
-			}
-		}
-	}
+	units []Unit
+}
+
+type Unit struct {
+	unitName string
+	pathways []Pathway
+}
+
+type Pathway struct {
+	pathwayName string
+	questions []Question
+}
+
+type Question struct {
+	questionTitle string
+	questionSubTitle string
+	questionType string
+	options []Option
+}
+
+type Option struct {
+	optionTitle string
+	correctness string
 }
 
 func main() {
@@ -38,21 +46,31 @@ func main() {
 
 	// обробка усіх pathway
 	for _, pathway := range pathways {
-		pathwayLink = pathway.MustProperty("href").String()
+		pathwayLink := pathway.MustProperty("href").String()
 
 		var unitNum int
 		var pathwayNum int
 		fmt.Sscanf(pathwayLink, "/courses/pathways/android-basics-compose-unit-%d-pathway-%d", &unitNum, &pathwayNum)
 		// TODO: add this data to structure
+		testData.units = append(testData.units, Unit{})
+		curUnit := &testData.units[len(testData.units)-1]
+		curUnit.unitName = "TodoUnitName"
+		curUnit.pathways = append(curUnit.pathways, Pathway{})
+		curPathway := &curUnit.pathways[len(curUnit.pathways)-1]
+		curPathway.pathwayName = "TodoPathwayName"
 
 		page := browser.MustPage(pathwayLink)
 		page.MustElement("div.devsite-playlist--item--actions:nth-child(3) > a:nth-child(1)").MustClick()
 
 		qs := page.MustWaitStable().MustElements("devsite-quiz-question")
 		for _, q := range qs {
-			qNum := q.MustProperty("data-index")
-			qTitle := q.MustElement("h2").MustText()
-			qSubTitle := q.MustElement("p").MustText()
+			curPathway.questions = append(curPathway.questions, Question{})
+			curQuestion := &curPathway.questions[len(curPathway.questions)-1]
+
+			curQuestion.questionTitle = q.MustElement("h2").MustText()
+			curQuestion.questionSubTitle = q.MustElement("p").MustText()
+			curQuestion.questionType = "TODO"
+			fmt.Println(curQuestion.questionSubTitle)
 		}
 		// TODO: add this data to structure
 
@@ -62,8 +80,8 @@ func main() {
 			bs := page.MustWaitStable().MustElements("input[value='" + strconv.Itoa(i) + "']");
 			for _, b := range bs {
 				var nq, nans int
-				fmt.Sscanf(cb.MustProperty("name").String(), "question-%d", &nq)
-				fmt.Sscanf(cb.MustProperty("value").String(), "%d",  &nans)
+				fmt.Sscanf(b.MustProperty("name").String(), "question-%d", &nq)
+				fmt.Sscanf(b.MustProperty("value").String(), "%d",  &nans)
 				// TODO: add this data to structure
 
 				b.MustClick()
